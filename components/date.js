0141b5223b072c109a085ca0b50204bf2164ca10
João Victor <joaovictor2@hotmail.com>
2021-04-27 00:23:40 -0300

    #1 feat - Commit Inicial

---
+++ components/date.js
@@ -0,0 +1,8 @@
+import { parseISO, format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{ format(date, 'PPPP', { locale: ptBR }) }</time>
+}
+
